57c6f15e8f4a71209229934b9b107e86380c2a81
hp <farahugaas0@gmail.com>
2025-05-25 22:14:54 +0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () =>{
         <footer className="bg-blue-600 text-white text-center py-6 mt-10">
             <p className="text-sm">© {new Date().getFullYear()} Abdulahi Ugaas. All rights reserved.</p>
             <div className="mt-2 space-x-4">
-                <a href="" className="hover:underline">Github</a>
+                <a href="https://github.com/farahos?tab=repositories" className="hover:underline">Github</a>
                 <a href="" className="hover:underline">LinkedIn</a>
             </div>
         </footer>
